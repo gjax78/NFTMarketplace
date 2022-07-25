0120ae08307a0c6bfdf5fcd3a681100e8a9a95fd
Geena Jackson <gjacks303@gmail.com>
2022-07-25 15:27:56 -0600

    Set up NFTcards

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Image, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 
-const NFTCard = () => {
+import { COLORS, SIZES, SHADOWS, assets } from '../constants'
+
+const NFTCard = ({data}) => {
+  const navigation = useNavigation()
+
   return (
-    <View>
-      <Text>NFTCard</Text>
+    <View style={{
+      backgroundColor: COLORS.white,
+      borderRadius: SIZES.font,
+      marginBottom: SIZES.extraLarge,
+      margin: SIZES.base,
+      ...SHADOWS.dark
+    }}>
+     <Text>NFTCard</Text>
     </View>
   )
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,20 +1,37 @@
 import { View, SafeAreaView, FlatList, Text } from 'react-native'
 import React, { useState } from 'react'
-import { COLORS, NFTdata } from '../constants'
+import { COLORS, NFTData } from '../constants'
 import { NFTCard, HomeHeader, FocusedStatusBar } from '../components'
 
-export default function Home() {
+const Home = () => {
   return (
    <SafeAreaView style={{ flex: 1 }}>
     <FocusedStatusBar background={COLORS.primary}/>
     <View style={{ flex: 1 }}>
       <View style={{ zIndex: 0}}>
         <FlatList 
-          data={NFTdata}
-          renderItem={({item}) => <Text>{item.name}</Text>}
+          data={NFTData}
+          renderItem={({item}) => <NFTCard data={item} />}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={<HomeHeader />}
         />
+      </View>
+
+      <View style={{ 
+          position: 'absolute',
+          top: 0,
+          bottom: 0, 
+          right: 0, 
+          left: 0, 
+          zIndex: -1, 
+        }}>
+          <View style={{height: 300, backgroundColor: COLORS.primary }}/>
+          <View style={{flex: 1, backgroundColor: COLORS.white}}/>
       </View>
     </View>
    </SafeAreaView>
   )
 }
+
+export default Home
