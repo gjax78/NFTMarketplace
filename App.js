5454c85d44c93bffe3cc9eed57e7252dbd67ea1b
Geena Jackson <gjacks303@gmail.com>
2022-07-25 13:14:05 -0600

    Add config files

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 import Home from './screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <Home />
+    <NavigationContainer>
+      <Home />
+    </NavigationContainer>
   );
 }
 
--- screens/Home.js
+++ screens/Home.js
@@ -7,6 +7,14 @@ export default function Home() {
   return (
    <SafeAreaView style={{ flex: 1 }}>
     <FocusedStatusBar background={COLORS.primary}/>
+    <View style={{ flex: 1 }}>
+      <View style={{ zIndex: 0}}>
+        <FlatList 
+          data={NFTdata}
+          renderItem={({item}) => <Text>{item.name}</Text>}
+        />
+      </View>
+    </View>
    </SafeAreaView>
   )
 }
