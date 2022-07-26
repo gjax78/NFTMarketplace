5b006b9ecd893e5ef6e588b2aca12722b46eb9a1
Geena Jackson <gjacks303@gmail.com>
2022-07-25 18:57:03 -0600

    Edit styling of buttons

--- App.js
+++ App.js
@@ -17,15 +17,15 @@ import { createStackNavigator } from "@react-navigation/stack";
 const Stack = createStackNavigator();
 
 const App = () => {
-  // const [loaded] = useFonts({
-  //   InterBold: require("./assets/fonts/Inter-Bold.ttf"),
-  //   InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
-  //   InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
-  //   InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
-  //   InterLight: require("./assets/fonts/Inter-Light.ttf"),
-  // });
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
 
-  // if (!loaded) return null;
+  if (!loaded) return null;
 
   return (
     <NavigationContainer >
