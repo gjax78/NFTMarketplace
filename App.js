31de1d27c80fa57ddcc964ee44014da452d496c2
Geena Jackson <gjacks303@gmail.com>
2022-07-25 18:50:47 -0600

    Add prices

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 import Home from './screens/Home';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, StackActions, DefaultTheme } from '@react-navigation/native';
+// import Details from "./screens/Details";
+import { useFonts } from "expo-font";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+// const theme = {
+//   ...DefaultTheme,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     background: "transparent",
+//   },
+// };
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  // const [loaded] = useFonts({
+  //   InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+  //   InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+  //   InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+  //   InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+  //   InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  // });
+
+  // if (!loaded) return null;
+
   return (
-    <NavigationContainer>
-      <Home />
+    <NavigationContainer >
+      <Stack.Navigator screenOptions={{
+      headerShown: false}}
+      initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} />
+      {/* <Stack.Screen name="Details" component={Details} /> */}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App
--- components/NFTCard.js
+++ components/NFTCard.js
@@ -4,6 +4,7 @@ import React from 'react'
 
 import { COLORS, SIZES, SHADOWS, assets } from '../constants'
 import { CircleButton, RectButton } from './Button'
+import { SubInfo, EthPrice, NFTTitle } from './SubInfo'
 
 const NFTCard = ({data}) => {
   const navigation = useNavigation()
@@ -29,6 +30,34 @@ const NFTCard = ({data}) => {
         />
 
         <CircleButton imgUrl={assets.heart} right={10} top={10}/>
+      </View>
+
+      <SubInfo />
+
+      <View
+        style={{
+          width: '100%',
+          padding: SIZES.font
+        }}>
+        <NFTTitle 
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+        <View style={{
+          marginTop: SIZES.top,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}>
+          <EthPrice price={data.price}/>
+          <RectButton 
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate('Details', { data })}
+          />
+        </View>
       </View>
     </View>
   )
